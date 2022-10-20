a40010fd5e3b72d2457d2e9b9eec07f9dd94ad6a
DESKTOP-RJC8TSU\taege <taegeun0525@gmail.com>
2022-10-20 09:28:53 +0900

    Stack01 Test

--- src/test/java/com/likelion/Stack01Test.java
+++ src/test/java/com/likelion/Stack01Test.java
@@ -1,6 +1,7 @@
 package com.likelion;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,29 +9,35 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 class Stack01Test {
-    @Test
-    void pushTest() {
-        Stack01 stack01 = new Stack01();
-        stack01.push(10);
-        stack01.push(20);
+    @BeforeEach
+    void setUp() {
+        // db를 지우는코드
+        System.out.println("before each");
+    }
 
-        int[] arr = stack01.getArr();
+    @Test
+    @DisplayName("push가 잘되는지")
+    void pushTest() {
+        Stack01 st = new Stack01();
+        st.push(10);
+        st.push(20);
+
+        int[] arr = st.getArr();
         //10,20
         Assertions.assertEquals(10, arr[0]);
         Assertions.assertEquals(20, arr[1]);
     }
 
     @Test
-    @DisplayName("push가 잘 되는지 pop이 잘 되는지")
     void pushAndPop() {
-        Stack01 stack01 = new Stack01();
-        stack01.push(10);
-        stack01.push(20);
+        Stack01 st = new Stack01();
+        st.push(10);
+        st.push(20);
 
-        Assertions.assertEquals(20,stack01.pop());
-        Assertions.assertEquals( 10,stack01.pop());
+        Assertions.assertEquals(20,st.pop());
+        Assertions.assertEquals( 10,st.pop());
 
-        stack01.push(30);
-        Assertions.assertEquals(30,stack01.pop());
+        st.push(30);
+        Assertions.assertEquals(30,st.pop());
     }
 }
